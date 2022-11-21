a4d9151a1acb9181e00ddfbe6f62d312f2394ad3
Jackline <jackline.muthama@student.moringaschool.com>
2022-11-21 20:02:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1=62 , num2=1
+let num3=24, num4=5
+const multiply = num1 * num2 
+const random=Math.ceil(Math.random())
+const mod = num3%num4
+const numbers =[10, 15, 17, 20]
+const max= Math.max(...numbers)
+    
